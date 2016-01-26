55e4e596f0b34e977b48234c66d5e1003250432e
Massimo Zappino <massimo.zappino@gmail.com>
2016-01-26 08:59:17 +0100

    wip

--- src/main/java/com/tagmycode/plugin/gui/form/SnippetView.java
+++ src/main/java/com/tagmycode/plugin/gui/form/SnippetView.java
@@ -2,6 +2,7 @@ package com.tagmycode.plugin.gui.form;
 
 import com.tagmycode.plugin.gui.AbstractGui;
 import com.tagmycode.plugin.gui.ClipboardCopy;
+import com.tagmycode.plugin.gui.SnippetEditorPane;
 import com.tagmycode.sdk.model.Snippet;
 
 import javax.swing.*;
@@ -12,14 +13,14 @@ import java.awt.event.ActionListener;
 public class SnippetView extends AbstractGui {
     private JButton copyButton;
     private JPanel mainPanel;
-    private JEditorPane snippetEditorPane;
+    private SnippetEditorPane snippetEditorPane;
     private JLabel title;
     private ClipboardCopy clipboardCopy;
 
     public SnippetView(Snippet snippet) {
         clipboardCopy = new ClipboardCopy();
         snippetEditorPane.setEditable(false);
-        snippetEditorPane.setText(snippet.getCode());
+        snippetEditorPane.setTextWithSnippet(snippet);
         getTitle().setText(snippet.getTitle());
 
         copyButton.addActionListener(new ActionListener() {
@@ -31,7 +32,7 @@ public class SnippetView extends AbstractGui {
         initPopupMenuForJTextComponents(getMainComponent());
     }
 
-    public JEditorPane getSnippetEditorPane() {
+    public SnippetEditorPane getSnippetEditorPane() {
         return snippetEditorPane;
     }
 
