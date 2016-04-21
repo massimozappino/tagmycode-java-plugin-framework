5c5293b40f043533851c48a7a78ea7d9f6660af5
Massimo Zappino <massimo.zappino@gmail.com>
2016-04-21 14:19:35 +0200

    JEditorPane is required for NetBeans

--- src/main/java/com/tagmycode/plugin/gui/SnippetEditorPane.java
+++ src/main/java/com/tagmycode/plugin/gui/SnippetEditorPane.java
@@ -4,7 +4,7 @@ import com.tagmycode.sdk.model.Snippet;
 
 import javax.swing.*;
 
-public class SnippetEditorPane extends JTextPane {
+public class SnippetEditorPane extends JEditorPane {
 
     public void setTextWithSnippet(Snippet snippet) {
         setText(snippet.getCode());
