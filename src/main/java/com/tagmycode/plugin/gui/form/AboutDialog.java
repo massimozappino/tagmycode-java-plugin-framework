dd322c33e0c3b3f650ec2f1532f34a5b03f4030a
Massimo Zappino <massimo.zappino@gmail.com>
2016-05-18 22:11:38 +0200

    added buttons to AboutDialog

--- src/main/java/com/tagmycode/plugin/gui/form/AboutDialog.java
+++ src/main/java/com/tagmycode/plugin/gui/form/AboutDialog.java
@@ -1,25 +1,46 @@
 package com.tagmycode.plugin.gui.form;
 
+import com.tagmycode.plugin.Browser;
 import com.tagmycode.plugin.Framework;
 import com.tagmycode.plugin.gui.AbstractDialog;
+import com.tagmycode.plugin.gui.GuiUtil;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AboutDialog extends AbstractDialog {
     private JComponent contentPane;
     private JButton buttonCancel;
-    private JTextArea textArea;
-    private JButton buttonOK;
+    private JButton licenseButton;
+    private JButton reportAnIssueButton;
+    private JLabel tagmycodeLinkLabel;
+    private JButton buttonOK = new JButton();
 
     public AboutDialog(Framework framework, Frame parent) {
         super(framework, parent);
-        buttonOK = new JButton();
-        textArea.setEnabled(false);
-        textArea.setBackground(contentPane.getBackground());
+
+        configureButtons();
+        GuiUtil.addClickableLink(tagmycodeLinkLabel, "https://tagmycode.com");
 
         defaultInitWindow();
         initWindow();
+    }
+
+    private void configureButtons() {
+        licenseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Browser().openUrl("http://www.apache.org/licenses/LICENSE-2.0");
+            }
+        });
+        reportAnIssueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Browser().openUrl("https://tagmycode.com/about/contact");
+            }
+        });
     }
 
     @Override
