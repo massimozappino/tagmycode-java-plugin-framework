6b6d27596a98da4c26adf9fb3d13d7588b29599a
Massimo Zappino <massimo.zappino@gmail.com>
2017-11-05 18:28:58 +0100

    restored old Browser class catch all Throwable exception including java.lang.UnsupportedOperationException: The BROWSE action is not supported on the current platform!

--- src/main/java/com/tagmycode/plugin/Browser.java
+++ src/main/java/com/tagmycode/plugin/Browser.java
@@ -1,25 +1,170 @@
 package com.tagmycode.plugin;
 
-import com.sun.deploy.uitoolkit.impl.fx.HostServicesFactory;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+enum EnumOS {
+    linux, macos, solaris, unknown, windows;
+
+    public boolean isLinux() {
+
+        return this == linux || this == solaris;
+    }
+
+    public boolean isMac() {
+
+        return this == macos;
+    }
+
+    public boolean isWindows() {
+
+        return this == windows;
+    }
+}
 
 public class Browser implements IBrowser {
 
-    private Application application;
+    private final EnumOS os;
 
     public Browser() {
-        this.application = new Application() {
-            @Override
-            public void start(Stage primaryStage) throws Exception {
-            }
-        };
+        os = guessOS();
     }
 
     @Override
     public boolean openUrl(String url) {
-        HostServicesFactory.getInstance(application).showDocument(url);
-        return true;
+        try {
+            return openURI(new URI(url));
+        } catch (URISyntaxException e) {
+            return false;
+        }
+    }
+
+    private boolean openURI(URI uri) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(uri);
+                return true;
+            } catch (Throwable e) {
+                return false;
+            }
+        } else {
+            return createOsBasedRule().browse(uri);
+        }
+    }
+
+    private Rule createOsBasedRule() {
+        if (os.isLinux()) {
+            return new LinuxRule();
+        }
+
+        if (os.isMac()) {
+            return new MacRule();
+        }
+
+        if (os.isWindows()) {
+            return new WindowsRule();
+        }
+
+        return new UnknownOsRule();
+
+    }
+
+    private EnumOS guessOS() {
+        String s = System.getProperty("os.name").toLowerCase();
+
+        if (s.contains("win")) {
+            return EnumOS.windows;
+        }
+
+        if (s.contains("mac")) {
+            return EnumOS.macos;
+        }
+
+        if (s.contains("solaris")) {
+            return EnumOS.solaris;
+        }
+
+        if (s.contains("sunos")) {
+            return EnumOS.solaris;
+        }
+
+        if (s.contains("linux")) {
+            return EnumOS.linux;
+        }
+
+        if (s.contains("unix")) {
+            return EnumOS.linux;
+        } else {
+            return EnumOS.unknown;
+        }
+    }
+
+    private class MacRule extends Rule {
+        @Override
+        public boolean browse(URI uri) {
+            return runCommand("open", "%s", uri.toString());
+        }
+    }
+
+    private class LinuxRule extends Rule {
+        @Override
+        public boolean browse(URI uri) {
+            return runCommand("xdg-open", "%s", uri.toString());
+        }
+    }
+
+    private class WindowsRule extends Rule {
+        @Override
+        public boolean browse(URI uri) {
+            return runCommand("explorer", "%s", uri.toString());
+        }
+    }
+
+    private class UnknownOsRule extends Rule {
+        @Override
+        public boolean browse(URI uri) {
+            return false;
+        }
+    }
+}
+
+abstract class Rule {
+    public abstract boolean browse(URI uri);
+
+    public boolean runCommand(String command, String args, String file) {
+        String[] parts = prepareCommand(command, args, file);
+
+        try {
+            Process p = Runtime.getRuntime().exec(parts);
+            if (p == null) return false;
+
+            try {
+                return p.exitValue() == 0;
+            } catch (IllegalThreadStateException e) {
+                return true;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private String[] prepareCommand(String command, String args, String file) {
+
+        java.util.List<String> parts = new ArrayList<>();
+        parts.add(command);
+
+        if (args != null) {
+            for (String s : args.split(" ")) {
+                s = String.format(s, file);
+
+                parts.add(s.trim());
+            }
+        }
+
+        return parts.toArray(new String[parts.size()]);
     }
 
 }
