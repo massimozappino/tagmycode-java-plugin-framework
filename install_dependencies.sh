2edb91e13a4836759bf73a055bc757f2793d585a
Massimo Zappino <massimo.zappino@gmail.com>
2019-11-25 11:23:42 +0100

    Fixing travis install_dependencies.sh with branch

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,14 +2,14 @@
 set -e
 
 sdk_project="tagmycode-java-sdk"
-branch=${BRANCH}
+BRANCH=${TRAVIS_BRANCH}
 [[ -z "$BRANCH" ]] && { echo "Error: parameter BRANCH is empty" ; exit 1; }
 
 rm -fr ${sdk_project}
 
-echo "Installing ${sdk_project}, branch ${branch}"
+echo "Installing ${sdk_project}, branch ${BRANCH}"
 
 git clone https://github.com/massimozappino/${sdk_project}.git
 cd ${sdk_project}
-git checkout ${branch}
+git checkout ${BRANCH}
 mvn install -DskipTests
